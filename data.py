0d3adf11a043f5d7790112dddc1b8915b164a68a
Xiaoyi Liu <jasonl@wustl.edu>
2023-06-19 18:23:52 -0500

    working on siamese implementation

---
+++ data.py
@@ -0,0 +1,13 @@
+data_folder_dictionary = {
+    'temperature_230509_discrete': "5.9.23/",
+    'pressure_230516_discrete': "5.16.23_Pressure/"
+}
+
+class data_location:
+    def __init__(self,
+                 input_kind,
+                 label_kind,
+                 folder,
+                 file_dictionary,
+                 ) -> None:
+        pass
